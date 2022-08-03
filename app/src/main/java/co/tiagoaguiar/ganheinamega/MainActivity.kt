4e69d1feda38d247c61320ff2285368280c3a480
Lucas Tabone <Lucas.tabone@hotmail.com>
2022-08-03 13:55:41 -0300

    Initial commit

---
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -0,0 +1,20 @@
+package co.tiagoaguiar.ganheinamega
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.edit_number)
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btnGenerate: Button = findViewById(R.id.btn_generate)
+        
+        txtResult.text = "ola jonhson"
+    }
+}
